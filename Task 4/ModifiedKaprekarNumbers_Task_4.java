61ca351f0c3fdf81ce2ac80e04f2056e71870376
Mohammed Ramadan <96293938+MohammedRamadanqawuq@users.noreply.github.com>
2022-02-13 22:01:56 +0200

    problem 7

--- Task 4/ModifiedKaprekarNumbers_Task_4.java
+++ Task 4/ModifiedKaprekarNumbers_Task_4.java
@@ -1,38 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package modified.kaprekar.numbers_task_4;
+import java.util.Scanner;
+public class JavaApplication12 {
 
-import java.util.*;
-
-public class ModifiedKaprekarNumbers_Task_4 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter lower number : ");
-        int p = input.nextInt();
-        System.out.println("Enter upper number : ");
-        int q = input.nextInt();
-        long num, remender, squar, sum;
-        for (int i = p; i <= q; i++) {
-            num = i;
-            squar = (long) Math.pow(num, 2);
-            sum = 0;
-            while (squar != 0) {
-                remender = squar % 10;
-                sum = sum + remender;
-                squar = squar / 10;
-
-            }
-            if (sum == num) {
-                System.out.println(i);
-            }
-        }
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int p = scan.nextInt();
+    int q = scan.nextInt();
+    boolean found = false;
+    for(long i = p; i <= q; i++) {
+        String sqr = Long.toString(i * i);
+        int d = sqr.length() - Long.toString(i).length();
+        int r = 0, l = 0;
+        if(d >= 0)
+            r = Integer.parseInt(sqr.substring(d));
+        if(d > 0)
+           l = Integer.parseInt(sqr.substring(0, d));
+        if(r + l == i) {
+            found = true;
+            System.out.print(i + " ");
+        }   
     }
 
+    if(!found)
+        System.out.println("INVALID RANGE");
+     }
 }
