fb0cb1f1a433d71ac878ce36d14b2c80a55f92c4
Mohammed Ramadan <96293938+MohammedRamadanqawuq@users.noreply.github.com>
2022-02-13 22:18:05 +0200

    problem 8

---
+++ Task 4/p8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class JavaApplication15 {
+
+public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    int t=in.nextInt();
+    for(int x=0;x<t;x++){
+        int n=in.nextInt();
+        int a=in.nextInt();
+        int b=in.nextInt();
+        if(a==b)System.out.print((n-1)*a);
+        else {
+            if(a>b){
+            int temp=a;
+            a=b;
+            b=temp;
+        }
+        for(int i=0;i<n;i++)System.out.print(i*b+(n-1-i)*a+" ");
+        }
+        System.out.println();
+    }
+      } 
+}
