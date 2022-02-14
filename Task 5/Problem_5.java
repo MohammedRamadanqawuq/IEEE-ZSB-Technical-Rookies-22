8976eeeb43843ddcf39bfbc862518810bd70f71e
Mohammed Ramadan <96293938+MohammedRamadanqawuq@users.noreply.github.com>
2022-02-14 18:33:06 +0200

    Problem 5

---
+++ Task 5/Problem_5.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+   
+  	public static void preOrder( Node root ) {
+      
+    	if( root == null)
+        	return;
+      
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+     
+    }
+	public static Node insert(Node root,int value) {
+        if(root==null)
+  return new Node(value);
+ else if(root.data>value)
+      root.left=insert(root.left,value);
+  else if(root.data<value)
+      root.right=insert(root.right,value);
+  return root;
+
+    	
+    }
+
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+      	preOrder(root);
+    }	
+}
